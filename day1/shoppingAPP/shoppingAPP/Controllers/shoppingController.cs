31a6b9400850bd8f84ce3200e6774e6f706833f9
Nikhil <nikhilshah.net@hotmail.com>
2022-12-03 13:19:53 +0530

    Day 1

---
+++ day1/shoppingAPP/shoppingAPP/Controllers/shoppingController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPP.Models;
+
+namespace shoppingAPP.Controllers
+{
+    public class shoppingController : Controller
+    {
+
+        public IActionResult ProductsList()
+        {
+            //collect the data from model
+            //pass the data to model
+            //ViewBag is used to pass data to views
+
+
+            //this is a bad practice, controller is not suppose to declare the data, it is suppose to take it from Model
+            ProductsModel pObj = new ProductsModel(); //this is bad code
+                                                      // we will use DI instead
+            
+            ViewBag.totalProducts = pObj.TotalProducts();
+            ViewBag.totalCategories = pObj.TotalCategories();
+            ViewBag.seller = pObj.Seller;
+
+            //List<string> categoryList = new List<string>()
+            //{
+            //    "Cold-Drinks","Electronics","Shoes","Accessories","TShirts","Cap"
+            //};
+
+            ViewBag.pCategories = pObj.GetDistinctCategories();
+
+            ViewBag.productList = pObj.GetAllProducts();
+
+            return View(); 
+        }
+
+        public IActionResult CustomerList()
+        {
+            //collect the data from model
+            //pass the data to model
+            //ViewBag is used to pass data to views
+            return View();
+        }
+        public IActionResult ViewCart()
+        {
+            //collect the data from model
+            //pass the data to model
+            //ViewBag is used to pass data to views
+            return View();
+        }
+        public IActionResult OrderHistory()
+        {
+            //collect the data from model
+            //pass the data to model
+            //ViewBag is used to pass data to views
+            return View();
+        }
+
+
+    }
+}
---
+++ day1/shoppingAPP/shoppingAPP/Models/ProductsModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+
+namespace shoppingAPP.Models
+{
+    public class ProductsModel
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+        public string Seller { get; set; } = "Nikhil Shah";
+
+        static List<ProductsModel> pList = new List<ProductsModel>()
+        {
+
+            new ProductsModel() { pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new ProductsModel() { pId=102, pName="Coke", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new ProductsModel() { pId=103, pName="Maggie", pCategory="Fast-Food", pIsInStock=false, pPrice=50},
+            new ProductsModel() { pId=104, pName="IPhone", pCategory="Electronics", pIsInStock=true, pPrice=50},
+            new ProductsModel() { pId=105, pName="Nike", pCategory="Shoes", pIsInStock=true, pPrice=50},
+            new ProductsModel() { pId=106, pName="Ray-Ban", pCategory="Accessories", pIsInStock=false, pPrice=50},
+            new ProductsModel() { pId=107, pName="Fossil", pCategory="Electronics", pIsInStock=true, pPrice=50},
+            new ProductsModel() { pId=108, pName="Dell lattitude", pCategory="Electronics", pIsInStock=false, pPrice=50},
+             new ProductsModel() { pId=109, pName="Ray-Ban", pCategory="Accessories", pIsInStock=false, pPrice=50},
+            new ProductsModel() { pId=110, pName="Fossil", pCategory="Electronics", pIsInStock=true, pPrice=50},
+            new ProductsModel() { pId=111, pName="Green-Tea", pCategory="Hot-Beverage", pIsInStock=false, pPrice=50},
+        };
+
+        public int TotalProducts()
+        {
+            return pList.Count;
+        }
+
+        public int TotalCategories()
+        {
+            var totalCategories = (from p in pList
+                                   select p.pCategory).Distinct().Count();
+
+            return totalCategories;
+        }
+
+        public List<string> GetDistinctCategories()
+        {
+            var distinctCategories = (from p in pList
+                                      select p.pCategory).Distinct().ToList();
+            return distinctCategories;
+        }
+
+        public List<ProductsModel> GetAllProducts()
+        {
+            return pList;
+        }
+
+    }
+}
