0dfb739377b890da3dc4db2305b4216879219dd3
Nikhil <nikhilshah.net@hotmail.com>
2022-12-15 11:34:10 +0530

    Docker deployment

---
+++ day9/firstmicroservicemvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace firstmicroservicemvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
